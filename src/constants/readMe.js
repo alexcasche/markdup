f69668b922677f39de4b54c6d8ef915ec926709b
alexanderasche <alexander.asche@gmail.com>
2018-07-31 16:42:18 -0400

    pkg homepage

--- src/constants/readMe.js
+++ src/constants/readMe.js
@@ -1,6 +1,6 @@
-export default `# MarkedUp
+export default `# MarkdUp
 
-MarkedUp is an online Markdown editor powered by React + Redux.
+MarkdUp is an online Markdown editor powered by React + Redux.
 
 ![Build Status][Travis-shield] ![JavaScript Style Guide][Prettier-shield] ![Packagist][MIT-shield]
  
